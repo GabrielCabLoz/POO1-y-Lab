f2f8c31ead72950cf8b319e2543fe54a8fe9d7c9
Gabriel Cabrera <Gabriel.c.l.05.01@hotmail.com>
2024-11-06 17:53:08 -0600

    Actividad 24

---
+++ P140_HolamundoGrafico.java
@@ -0,0 +1,31 @@
+import java.awt.Font;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class P140_HolamundoGrafico extends JFrame {
+
+    private JLabel lblsaludo;
+    
+    public P140_HolamundoGrafico(){
+
+        setLayout(null);
+        lblsaludo=new JLabel("Hola mundo Cruel "); //Texto en la ventana 
+        lblsaludo.setFont(new Font("Times New Roman",Font.BOLD, 30));
+        lblsaludo.setBounds(270,100,400,50); //Es el tamaño de la etiqueta
+        add(lblsaludo); //Se agrega a la ventana
+        setTitle("Mi primer programa en Interfaz grafica ");//Titulo de la ventana 
+    }
+
+    public static void main(String[] args) {
+        P140_HolamundoGrafico Main=new P140_HolamundoGrafico();
+        Main.setBounds(20,20,800,300);
+        Main.setLocationRelativeTo(null);
+        Main.setLocale(null);
+        Main.setVisible(true);
+        Main.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+        
+
+    
+    
+}
---
+++ p141_AreaTriangulo.java
@@ -0,0 +1,86 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class p141_AreaTriangulo extends JFrame implements ActionListener {
+
+    JLabel lblBase,lblAltura,lblResultado;
+    JTextField txtBase,txtAltura,txtResultado;
+    JButton btnCalcular,btnSalir;
+
+    public p141_AreaTriangulo(){
+        super("Calcular el area del triangulo");
+        setLayout(null);
+
+        lblBase=new JLabel("Base");
+        lblBase.setBounds(10,20,200,30);
+        add(lblBase);
+        txtBase=new JTextField();
+        txtBase.setBounds(80,20,200,30);
+        add(txtBase);
+
+        lblAltura=new JLabel("Altura");
+        lblAltura.setBounds(10,50,200,30);
+        add(lblAltura);
+        txtAltura=new JTextField();
+        txtAltura.setBounds(80,50,200,30);
+        add(txtAltura);
+
+        lblResultado=new JLabel("Resultado aqui ");
+        lblResultado.setBounds(90,80,200,30);
+        add(lblResultado);
+
+        lblResultado=new JLabel("Calcular ");
+        lblResultado.setBounds(90,80,100,30);
+        add(lblResultado);
+
+        lblResultado=new JLabel("Salir ");
+        lblResultado.setBounds(150, 100,100,40);
+        add(lblResultado);
+
+        btnCalcular.addActionListener(this);
+        btnSalir.addActionListener(this);
+
+        
+    }
+
+    public float Calculararea(float base ,float altura){
+        return (base*altura)/2;
+
+    }
+
+
+    public void actionPerformed(ActionEvent e){
+
+        if (e.getSource()==btnCalcular){
+
+            float base=Float.parseFloat(txtBase.getText());
+            float altura=Float.parseFloat(txtAltura.getText());
+
+            float area=Calculararea(base,altura);
+
+            lblResultado.setText(String.format("%.2f",area));
+        }else if (e.getSource()==btnSalir);{
+            
+            JOptionPane.showMessageDialog(this, "Ya caile compa");
+            dispose();
+
+        }
+
+        
+    }
+
+    public static void main(String[] args) {
+
+        p141_AreaTriangulo app =new p141_AreaTriangulo();
+        app.setBounds(10,10,300,20);
+        app.setLocationRelativeTo(null);
+        app.setVisible(true);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+
+        
+    }
+
+
+    
+}
